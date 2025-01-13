f65f80810ae04e39dc381e93a90794a0444d459e
yeol@naver.com <yeol5185@naver.com>
2025-01-13 22:04:09 +0900

    update

---
+++ cote/src/cote/N_plus.java
@@ -0,0 +1,25 @@
+package cote;
+
+public class N_plus {
+
+	/*
+	 * 정수 배열 numbers와 정수 n이 매개변수로 주어집니다. 
+	 * numbers의 원소를 앞에서부터 하나씩 더하다가 
+	 * 그 합이 n보다 커지는 순간 이때까지 더했던 원소들의 합을 
+	 * return 하는 solution 함수를 작성해 주세요.
+	 */
+	
+	    public int solution(int[] numbers, int n) {
+	        int sum = 0;
+	        
+	        for (int num : numbers) {
+	            sum += num;
+	            if (sum > n) {
+	                return sum;
+	            }
+	        }
+	        
+	        return sum; 
+	    }
+	}
+
