f954d86563ef99093ce4e99cd1a315557e51ad35
yeol@naver.com <yeol5185@naver.com>
2025-01-22 23:18:59 +0900

    update

--- cote/src/sql/test4.sql
+++ cote/src/sql/test4.sql
@@ -10,12 +10,14 @@ DATETIME	DATETIME	FALSE
 INTAKE_CONDITION	VARCHAR(N)	FALSE
 NAME	VARCHAR(N)	TRUE
 SEX_UPON_INTAKE	VARCHAR(N)	FALSE
---동물 보호소에 가장 먼저 들어온 동물의 이름을 조회하는 SQL 문을 작성해주세요.
+--동물 보호소에 들어온 모든 동물의 아이디와 이름, 보호 시작일을 이름 순으로 
+--조회하는 SQL문을 작성해주세요. 
+--단, 이름이 같은 동물 중에서는 보호를 나중에 시작한 동물을 먼저 보여줘야 합니다.
 
-SELECT
-    NAME
-FROM
+SELECT 
+    ANIMAL_ID, NAME, DATETIME
+FROM 
     ANIMAL_INS
-ORDER BY
-    DATETIME
-LIMIT 1
+ORDER BY 
+    NAME, 
+    DATETIME DESC
