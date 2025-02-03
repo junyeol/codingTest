e2a8e8525b02aef3e4d8c9cd993bfb1c09ffb391
yeol@naver.com <yeol5185@naver.com>
2025-02-03 22:44:51 +0900

    update

---
+++ cote/src/cote/Num.java
@@ -0,0 +1,18 @@
+package cote;
+
+public class Num {
+
+	//정수 num과 n이 매개 변수로 주어질 때,
+	//num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 
+	//return하도록 solution 함수를 완성해주세요.
+	
+	 public int solution(int num, int n) {
+	        int answer = 0;
+
+	        if(num % n == 0){
+	            return 1;
+	        }
+
+	        return answer;
+	    }
+}
