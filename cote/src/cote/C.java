c92cbf6f4a40473665afc7221850bfd923c41b4a
yeol@naver.com <yeol5185@naver.com>
2025-03-16 10:49:46 +0900

    update

--- cote/src/cote/C.java
+++ cote/src/cote/C.java
@@ -4,6 +4,7 @@ public class C {
 
 	//정수 num1, num2가 매개변수로 주어질 때, 
 	//num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+	//C++
 	
 	
 	int solution(int num1, int num2) {
@@ -13,7 +14,7 @@ public class C {
 
 	int main() {
 	    int num1 = 10, num2 = 3;
-	    cout << solution(num1, num2) << endl;  // 출력: 3
+	    cout << solution(num1, num2) << endl;
 	    return 0;
 	}
 }
