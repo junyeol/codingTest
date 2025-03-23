8acf7adaa1d1c72071531782672333c512ce18b7
yeol@naver.com <yeol5185@naver.com>
2025-03-23 18:05:42 +0900

    update

--- cote/src/cote/Change.java
+++ cote/src/cote/Change.java
@@ -4,9 +4,10 @@ public class Change {
 		//숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 
 		//return하도록 solution 함수를 완성해주세요
 	
-	    public int solution(String n_str) {
+	    public int solution(String n_str) {//문자열 _str
 	        int answer = 0;
-	        answer = Integer.parseInt(n_str);//형변환
+	        
+	        answer = Integer.parseInt(n_str);//형변환 실행!
 	        
 	        return answer;
 	    }
