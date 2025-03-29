c88fda95fccbfe95d28aede2a00be5b718d01178
yeol@naver.com <yeol5185@naver.com>
2025-03-29 23:17:10 +0900

    update

--- cote/bin/sql/test1.sql
+++ cote/bin/sql/test1.sql
@@ -13,6 +13,6 @@ SEX_UPON_INTAKE	VARCHAR(N)	FALSE
 --동물 보호소에 들어온 모든 동물의 아이디와 이름을 ANIMAL_ID순으로 조회하는 SQL문을 작성해주세요. 
 --SQL을 실행하면 다음과 같이 출력되어야 합니다.
 
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS
-ORDER BY ANIMAL_ID
+SELECT ANIMAL_ID, NAME --동물의 아이디와 이름
+FROM ANIMAL_INS --해당 테이블에서
+ORDER BY ANIMAL_ID --ANIMAL_ID순으로 조회
