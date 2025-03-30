927d7c90661b29c0209ab353059d358b445b7f22
yeol@naver.com <yeol5185@naver.com>
2025-03-30 22:37:04 +0900

    update

--- cote/src/cote/Num.java
+++ cote/src/cote/Num.java
@@ -7,10 +7,10 @@ public class Num {
 	//return하도록 solution 함수를 완성해주세요.
 	
 	 public int solution(int num, int n) {
-	        int answer = 0;
+	        int answer = 0;//기본 0
 
-	        if(num % n == 0){
-	            return 1;
+	        if(num % n == 0){//num을 n으로 나누었을때 0이라면
+	            return 1;//1 리턴
 	        }
 
 	        return answer;
